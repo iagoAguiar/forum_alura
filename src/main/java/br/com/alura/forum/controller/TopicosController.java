29481e20f008459a4b9db018e4da92612c423e39
iagoAguiar <iagoaguiar202@gmail.com>
2021-02-06 18:47:15 -0300

    Usando Spring Data

--- src/main/java/br/com/alura/forum/controller/TopicosController.java
+++ src/main/java/br/com/alura/forum/controller/TopicosController.java
@@ -1,30 +1,39 @@
 package br.com.alura.forum.controller;
 
-import java.util.Arrays;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import br.com.alura.forum.controller.dot.TopicoDto;
-import br.com.alura.forum.modelo.Curso;
+import br.com.alura.forum.controller.dto.TopicoDto;
 import br.com.alura.forum.modelo.Topico;
+import br.com.alura.forum.repository.TopicoRepository;
+
+
 
 @RestController
 public class TopicosController {
 	
 	
+	@Autowired
+	private TopicoRepository topicoRepository;
+	
 	@RequestMapping("/topicos")
-	public List<TopicoDto> lista(){
-		
-		Topico topico = new Topico("Dúvida", "Dúvida com Spring", new Curso("Spring", "Programação"));
-		Topico topico1 = new Topico("Dúvida", "Dúvida com React", new Curso("React", "Front end"));
-		Topico topico2 = new Topico("Dúvida", "Dúvida com Python", new Curso("Data Science", "Data Science"));
-		Topico topico3 = new Topico("Dúvida", "Dúvida com Javascript", new Curso("Javascript", "Programação"));
-		
+	public List<TopicoDto> lista(String nomeCurso){
 		
 
-		return  TopicoDto.converter(Arrays.asList(topico, topico1, topico2));
+		
+		if(nomeCurso == null) {
+			List<Topico> topicos = topicoRepository.findAll();
+			return  TopicoDto.converter(topicos);
+			
+		}else {
+			List<Topico> topicos = topicoRepository.findByCurso_Nome(nomeCurso);
+			return  TopicoDto.converter(topicos);			
+		}
+		
+
 		
 	}
 
---
+++ src/main/java/br/com/alura/forum/repository/TopicoRepository.java
@@ -0,0 +1,21 @@
+package br.com.alura.forum.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import br.com.alura.forum.modelo.Topico;
+
+
+
+public interface TopicoRepository extends JpaRepository<Topico, Long> {
+
+	List<Topico> findByCurso_Nome(String nomeCurso);
+	
+	@Query("Select t from Topico t where t.curso.nome= :nomeCurso")
+	List<Topico> carregarPorNomeDoCurso(String nomeCurso);
+	
+	
+
+}
